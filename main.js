40559dc6be0e68e3252bcda766368f4fa519c819
Osama Khan <osamaa.khan61@gmail.com>
2021-02-18 13:23:05 -0800

    Added event listener for arrow keys #2

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 var $f1Car = document.querySelector('#f1-car-model');
+
+document.onkeydown = keyPress;
+
+function keyPress(press) {
+
+  press = press || window.event;
+
+  if (press.keyCode === 38) {
+    console.log('up arrow');
+  } else if (press.keyCode === 40) {
+    console.log('down arrow');
+  } else if (press.keyCode === 37) {
+    console.log('left arrow');
+  } else if (press.keyCode === 39) {
+    console.log('right arrow');
+  }
+}
