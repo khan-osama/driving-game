7c0f9474c2af786282af4fb3832fc5a79b12dec4
Osama Khan <osamaa.khan61@gmail.com>
2021-02-18 13:43:47 -0800

    Added css classes for each transformation and edited function to rotate car on keypress #2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var $f1Car = document.querySelector('#f1-car-model');
+var $f1Car = document.querySelector('.f1-car-model');
 
 document.onkeydown = keyPress;
 
@@ -6,13 +6,13 @@ function keyPress(press) {
 
   press = press || window.event;
 
-  if (press.keyCode === 38) {
-    console.log('up arrow');
-  } else if (press.keyCode === 40) {
-    console.log('down arrow');
-  } else if (press.keyCode === 37) {
-    console.log('left arrow');
-  } else if (press.keyCode === 39) {
-    console.log('right arrow');
+  if (press.keyCode === 38) { // up
+    $f1Car.className = 'f1-car-model-up';
+  } else if (press.keyCode === 40) { // down
+    $f1Car.className = 'f1-car-model-down';
+  } else if (press.keyCode === 37) { // left
+    $f1Car.className = 'f1-car-model-left';
+  } else if (press.keyCode === 39) { // right
+    $f1Car.className = 'f1-car-model-right';
   }
 }
